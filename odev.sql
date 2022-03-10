e1325803762941d57e00357e666f1067df8a4214
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2022-03-10 22:52:49 +0300

    Update odev.sql
    Odev 2

--- odev.sql
+++ odev.sql
@@ -1,3 +1,4 @@
+/*Odev 1*/
 /*Soru 1*/ 
 SELECT title,description from film
 /*Soru 2*/
@@ -8,3 +9,11 @@ Select * from film Where (rental_rate = 0.99 and replacement_cost = 12.99 or rep
 Select last_name from customer Where first_name = 'Mary'
 /*Soru 5*/
 Select * from film Where not (length < 50) and  (rental_rate = 2.99 or rental_rate = 4.99 )
+
+/*Odev 2*/
+/*Soru 1*/
+Select * from film Where replacement_cost Between 12.99 and 16.99
+/*Soru 2*/
+Select first_name,last_name from actor Where first_name in ('Penelope','Nick','Ed')
+/*Soru 3*/
+Select * from film Where rental_rate in (0.99, 2.99, 4.99) and  replacement_cost in (12.99, 15.99, 28.99)
