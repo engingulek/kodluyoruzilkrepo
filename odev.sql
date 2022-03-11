4c7729453450442b89973c42ccb046a8c530222e
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2022-03-11 18:22:02 +0300

    Update odev.sql
    Soru 10

--- odev.sql
+++ odev.sql
@@ -140,3 +140,14 @@ Select  payment.payment_id,customer.first_name,customer.last_name from customer 
 /*Soru 3*/
 Select rental.rental_id,customer.first_name,customer.last_name from customer Join rental on customer.customer_id = rental.customer_id
 
+
+/*Soru 10*/
+/*Soru 1*/
+Select city.city, country.country from city Left join country on city.country_id = country.country_id
+/*Soru 2*/
+Select payment_id, customer.first_name, customer.last_name 
+from customer Right join payment on customer.customer_id = payment.customer_id;
+/*Soru 3*/
+Select rental.rental_id , customer.first_name, customer.last_name from customer 
+Full join  rental on rental.customer_id = customer.customer_id
+
