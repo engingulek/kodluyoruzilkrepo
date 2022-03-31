77806d55959ac44b8a0620eeb2d6a2f1ea7b996f
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2022-04-01 02:19:03 +0300

    Node.js Ödevi 1

---
+++ nodeJSÖdev1.js
@@ -0,0 +1,10 @@
+let arg= process.argv.slice(2);
+
+
+const daireAlanı = (yaricap) => {
+    const piSayisi = Math.PI
+    const sonuc = piSayisi * yaricap**2;
+    console.log(sonuc)
+}
+
+circleArea(arg[0]*1);
